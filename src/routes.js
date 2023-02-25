8a0480a86dfb31ed284709c8dee1316702c1021c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-02-25 15:27:32 -0300

    create PagamentoProvider

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { CarrinhoProvider } from 'common/context/Carrinho';
+import { PagamentoProvider } from 'common/context/Pagamento';
 import { UsuarioProvider } from 'common/context/Usuario';
 import Carrinho from 'pages/Carrinho';
 import Feira from 'pages/Feira';
@@ -19,9 +20,11 @@ function Routes() {
             <Route path='/feira'>
               <Feira />
             </Route>
-            <Route path='/carrinho'>
-              <Carrinho />
-            </Route>
+            <PagamentoProvider>
+              <Route path='/carrinho'>
+                <Carrinho />
+              </Route>
+            </PagamentoProvider>
           </CarrinhoProvider>
         </UsuarioProvider>
 
